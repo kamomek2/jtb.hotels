973fc56301d5600252078b5024dc11fdca2d0ffe
muerwre <gotham48@gmail.com>
2019-04-08 17:35:46 +0700

    added index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as flightReducer, flightPersistConfig } from './redux/flight/reducer';
+export { default as flightSaga } from './redux/flight/sagas';
+export { FLIGHT_ACTIONS } from "./redux/flight/constants";
+export * from './redux/flight/actions';
+export { default as FlightList } from './containers/FlightList';
+export { IFlightState } from './redux/flight/reducer';
