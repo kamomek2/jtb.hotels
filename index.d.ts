cc22e1bcc29de5eecac75c31bd8e7eac1baec261
muerwre <gotham48@gmail.com>
2019-04-11 12:07:22 +0700

    fixed type export

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { Reducer } from "redux";
 import { PersistConfig } from "redux-persist/es/types";
 import * as React from "react";
-import * as Types from "src/types";
+import * as Types from "./src/types";
 
 declare const hotelReducer: Reducer;
 declare const hotelPersistConfig: PersistConfig;
